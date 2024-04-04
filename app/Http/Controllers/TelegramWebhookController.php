ba19e3e91f025b106827f3eaed686f0532356619
NicatG <nicatteymurov88@gmail.com>
2024-04-04 12:34:03 +0400

    first commit

---
+++ app/Http/Controllers/TelegramWebhookController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TelegramWebhookController extends Controller
+{
+    public function webhook(Request $request)
+    {
+    
+        return  $request;
+    }
+}
