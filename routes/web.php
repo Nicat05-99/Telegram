7e000106f9c5c3146f1d1c4ddb66179fa0ff014c
NicatG <nicatteymurov88@gmail.com>
2024-04-16 10:40:32 +0400

    nicat

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Route::match(['post', 'get'],'/telegramWebhook', [TelegramWebhookController::cla
 Route::get('/insert',[TelegramWebhookController::class,'webhook']);
 Route::get('/test', function () {
     echo "Merhaba, bu bir test sayfasıdır.";
-    
+
+    //nicat
 });
